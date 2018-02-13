c08a4bbd272c517d20cad44588f86fc9d574e3a5
Josi Moore <josib.moore@gmail.com>
2018-02-13 14:51:39 -0700

    trying to keep the code dry...

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import UserList from './components/UserList/UserList';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<UserList />, document.getElementById('root'));
 registerServiceWorker();
