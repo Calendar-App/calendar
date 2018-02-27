3136736f04d4b4d9a27c3a07dcfeeb2ebea4f1f6
Thomas Lowry <minilao94@yahoo.com>
2018-02-27 11:40:04 -0700

    added box-shadow to months and removed serviceworker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import AdminView from './components/AdminView/AdminView';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
